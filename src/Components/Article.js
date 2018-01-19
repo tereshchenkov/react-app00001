fb2a88acd2b6cdbfb219bf916944043e91cd1ca7
Vitali Tereshchenkov <vitalikgaga@gmail.com>
2018-01-19 21:40:55 +0300

    com

--- src/Components/Article.js
+++ src/Components/Article.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import CommentList from './CommentList'
 
 export default class Article extends Component {
     constructor(props) {
@@ -12,7 +13,12 @@ export default class Article extends Component {
     getBody() {
         if (!this.state.isOpen) return null
         const {article} = this.props;
-        return <section>{article.text}</section>
+        return (
+            <div>
+              <section>{article.text}</section>
+              {article.comments ? <CommentList comments = {article.comments} /> : null}
+            </div>
+        )
     }
 
     toggleOpen = () => {
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
 import {render} from 'react-dom'
 import {articles} from './fixtures'
-import ArticleList from './ArticleList'
+import ArticleList from './Components/ArticleList'
 render(<ArticleList articles = {articles} />, document.getElementById('container') );
